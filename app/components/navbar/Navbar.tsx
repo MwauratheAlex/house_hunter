eb128c834295655ace5a8fe5fd9302699f84489c
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-02 11:38:19 +0300

    Responsive navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,21 +1,24 @@
 import styles from "../../styles/navbar.module.css";
 import Link from "./Link";
-import logo from "./images/logo.png";
+import logoImg from "./images/logo.svg";
 import Image from "next/image";
 
 export default async function Navbar() {
-  const { nav, authBtns, underline, signUp, border } = styles;
+  const { nav, authBtns, underline, border, logo, navElContainer } = styles;
 
   return (
     <nav className={nav}>
-      <Image src={logo} alt="House Hunter logo" />
-      <ul>
-        <Link text="List your property" variant={underline} />
-        <div className={authBtns}>
-          <Link text="Login" />
-          <Link text="Sign up" variant={border} />
-        </div>
-      </ul>
+      <div className={navElContainer}>
+        <Image src={logoImg} alt="House Hunter logo" className={logo} />
+
+        <ul>
+          <Link text="List your property" variant={underline} />
+          <div className={authBtns}>
+            <Link text="Login" />
+            <Link text="Sign up" variant={underline} />
+          </div>
+        </ul>
+      </div>
     </nav>
   );
 }
