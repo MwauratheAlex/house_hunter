b12b63a687b9d1ccd70e0ca12bee4c00bcfc0da7
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-02 12:41:51 +0300

    Responsive search bar: good enough

--- app/components/search/SearchBtn.tsx
+++ app/components/search/SearchBtn.tsx
@@ -5,7 +5,7 @@ import styles from "../../styles/search.module.css";
 export default async function SearchBtn() {
   return (
     <div className={styles.btn}>
-      <button>Search</button>
+      <button>More filters</button>
       <Image src={searchIcon} alt="" width={29} height={29} />
     </div>
   );
