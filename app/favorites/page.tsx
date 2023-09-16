6645b25924fe9e53473ede2f975a84578b584f06
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-16 21:31:55 +0300

    refactor

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,20 +1,23 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getFavoriteListings from "@/app/actions/getFavoriteListings";
 import ListingCard from "../components/listingCard/ListingCard";
+import ClientOnly from "../components/ClientOnly";
 
 export default async function page() {
   const listings = await getFavoriteListings();
   const currentUser = await getCurrentUser();
 
   return (
-    <main>
-      {listings.map((listing: any) => (
-        <ListingCard
-          data={listing}
-          key={listing.id}
-          currentUser={currentUser}
-        />
-      ))}
-    </main>
+    <ClientOnly>
+      <main>
+        {listings.map((listing: any) => (
+          <ListingCard
+            data={listing}
+            key={listing.id}
+            currentUser={currentUser}
+          />
+        ))}
+      </main>
+    </ClientOnly>
   );
 }
