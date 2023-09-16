e752152889756294c561d2a1d537d1315f4be902
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-16 17:18:52 +0300

    RentModal: Location UI

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -58,17 +58,51 @@ const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
   //
 
   return (
-    <div className="relative">
+    <div className="w-full relative">
       <input
         type="text"
-        placeholder="Anywhere"
+        placeholder=" "
         value={searchSting} // Use typedValue as the input value
         onChange={(e) => setSearchString(e.target.value)} // Update typedValue on input change
-        className="text-lg p-3 border-2"
+        // className="text-lg p-3 border-2"
+        className={`
+          peer
+          w-full
+          p-4
+          pt-6
+          font-light
+          bg-white
+          border-2
+          rounded-md
+          outline-none
+          transition
+          disabled:opacity-70
+          disabled:cursor-not-allowed`}
       />
 
+      <label
+        className={`
+        absolute
+        text-md
+        duration-150
+        transform
+        -translate-y-3
+        top-5
+        left-4
+        z-10
+        origin-[0]
+        peer-placeholder-shown:scale-100
+        peer-placeholder-shown:translate-y-0
+        peer-focus:scale-75
+        peer-focus:-translate-y-4
+        text-zinc-400
+      `}
+      >
+        Anywhere
+      </label>
+
       {results.length > 0 && (
-        <div className="flex flex-col items-start gap-3 bg-gray-100 fixed z-[10000] w-fit p-3 pr-10 rounded-md cursor-pointer ">
+        <div className="flex flex-col items-start gap-3 bg-gray-50 fixed z-[10000] w-fit p-3 pr-10 rounded-md cursor-pointer ">
           {results.map((result) => (
             <p
               className="text-neutral-500 ml-1 'text-lg'"
